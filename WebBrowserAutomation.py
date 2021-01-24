1c5d9b2377cfc921226f86a8168f4ce2f211b756
lufherna <rojoclifford@gmail.com>
2021-01-24 16:26:09 -0500

    added correct code in order to run the suite of test cases. Now will focus on creating additional test cases for different pages

--- WebBrowserAutomation.py
+++ WebBrowserAutomation.py
@@ -1,29 +1,34 @@
 import unittest
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-chromeDriver = webdriver.Chrome("./chromedriver")
-
 
 # this class will test the main page login ONLY
 class mainLoginPageTest(unittest.TestCase):
 
-    def enterCorrectUNandPW(self):
+    def setUp(self):
+        self.driver = webdriver.Chrome("./chromedriver")
+        self.driver.maximize_window()
         # accesses test website
-        chromeDriver.get("https://parabank.parasoft.com/parabank/index.htm")
+        self.driver.get("https://parabank.parasoft.com/parabank/index.htm")
+
+    def testLoginInputs(self):
         # grab username input
-        usernameInput = chromeDriver.find_element_by_name('username')
+        usernameInput = self.driver.find_element_by_name('username')
         # entering a username to login
         usernameInput.send_keys('john')
-
         # grab password input
-        passwordInput = chromeDriver.find_element_by_name('password')
+        passwordInput = self.driver.find_element_by_name('password')
         # entering a password to login
         passwordInput.send_keys('demo')
         # clicking on 'Login'
-        loginButton = chromeDriver.find_element_by_xpath("//input[@value='Log In']")
+        loginButton = self.driver.find_element_by_xpath("//input[@value='Log In']")
         loginButton.click()
 
-    # closes the browser window
-    chromeDriver.close()
+    # def tearDown(self):
+    #     self.driver.quit()
+
+# runs the suite of tests mentioned above
+if __name__ == '__main__':
+    unittest.main()
+
+
