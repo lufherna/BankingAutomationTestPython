c5a067d6e604a45d019403eb8f789aa14f1b8a19
lufherna <rojoclifford@gmail.com>
2021-01-24 15:29:09 -0500

    Added additional code to grab username, password and click on the login button.

--- WebBrowserAutomation.py
+++ WebBrowserAutomation.py
@@ -1,9 +1,29 @@
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-chromeDriver = webdriver.Chrome("/chromedriver")
+chromeDriver = webdriver.Chrome("./chromedriver")
 
-# accesses test website
-chromeDriver.get("https://parabank.parasoft.com/parabank/index.htm")
-print(chromeDriver.title)
 
+# this class will test the main page login ONLY
+class mainLoginPageTest(unittest.TestCase):
+
+    def enterCorrectUNandPW(self):
+        # accesses test website
+        chromeDriver.get("https://parabank.parasoft.com/parabank/index.htm")
+        # grab username input
+        usernameInput = chromeDriver.find_element_by_name('username')
+        # entering a username to login
+        usernameInput.send_keys('john')
+
+        # grab password input
+        passwordInput = chromeDriver.find_element_by_name('password')
+        # entering a password to login
+        passwordInput.send_keys('demo')
+        # clicking on 'Login'
+        loginButton = chromeDriver.find_element_by_xpath("//input[@value='Log In']")
+        loginButton.click()
+
+    # closes the browser window
+    chromeDriver.close()
