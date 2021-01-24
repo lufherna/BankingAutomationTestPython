516c83a55af6de76550e23ea10e376c455448f4d
lufherna <rojoclifford@gmail.com>
2021-01-24 13:38:23 -0500

    Added code to access test website

--- WebBrowserAutomation.py
+++ WebBrowserAutomation.py
@@ -1,1 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chromeDriver = webdriver.Chrome("/chromedriver")
+
+# accesses test website
+chromeDriver.get("https://parabank.parasoft.com/parabank/index.htm")
+print(chromeDriver.title)
+
